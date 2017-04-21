01e1e49e679435422e2695ebefe77e11fec10d75
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-04-21 18:34:56 -0400

    Moved the JSON Data to Service

--- app/twitter.component.ts
+++ app/twitter.component.ts
@@ -23,15 +23,15 @@ import {TwitterService} from './twitter.service'
 export class TwitterComponent{
     @Input() isFavorite=false;
     count=10;
-   @Output() change = new EventEmitter();
-    inputData=[
-        {id:0,name:'Mosh',accountName:'Msadasdasd',tagLine:'There is only one tage line for Mosh, Still waiting add the correct TagLine',likeCount:10,isLiked:false},
-        {id:1,name:'Feng',accountName:'Kardsadaa',tagLine:'There is only one tage line for Feng, Still waiting add the correct TagLine',likeCount:90,isLiked:false},
-        {id:2,name:'Bykrz',accountName:'Dersdaanda',tagLine:'There is only one tage line for Bykr, Still waiting add the correct TagLine',likeCount:1,isLiked:false},
-        {id:3,name:'Doug',accountName:'Sterweanda',tagLine:'There is only one tage line for Doug, Still waiting add the correct TagLine',likeCount:75,isLiked:false}
-    ];
-onLikeClick(newValue)
-{
+    @Output() change = new EventEmitter();
+    inputData: any[];
+    
+    constructor(twitterService:TwitterService){
+         this.inputData=twitterService.getTweets(); 
+   }
+
+onLikeClick(newValue){
+    
      console.log("Start of onLikeClick: Index ="+newValue+
          ": Count ="+this.inputData[newValue].likeCount+" : isLiked = "+this.inputData[newValue].isLiked);
      this.inputData[newValue].isLiked = !this.inputData[newValue].isLiked;
--- app/twitter.service.ts
+++ app/twitter.service.ts
@@ -1,10 +1,12 @@
 export class TwitterService
 {
-    /*
-inputData=[
-    {id:1,name:'Yashwanth',accountName:'yreddybanda',tagLine:'There is only one tage line for Yashwanth'},
-{id:2,name:'Harish',accountName:'hreddybanda',tagLine:'There is only one tage line for Harish'},
-{id:3,name:'Venkat',accountName:'vreddybanda',tagLine:'There is only one tage line for Venkat'},
-{id:4,name:'Sunanda',accountName:'sreddybanda',tagLine:'There is only one tage line for Sunanda'}
-];*/
+    getTweets(){
+        return [ 
+                 {id:0,name:'Mosh',accountName:'Msadasdasd',tagLine:'There is only one tage line for Mosh, Still waiting add the correct TagLine',likeCount:10,isLiked:false},
+                 {id:1,name:'Feng',accountName:'Kardsadaa',tagLine:'There is only one tage line for Feng, Still waiting add the correct TagLine',likeCount:90,isLiked:true},
+                 {id:2,name:'Bykrz',accountName:'Dersdaanda',tagLine:'There is only one tage line for Bykr, Still waiting add the correct TagLine',likeCount:1,isLiked:true},
+                 {id:3,name:'Doug',accountName:'Sterweanda',tagLine:'There is only one tage line for Doug, Still waiting add the correct TagLine',likeCount:75,isLiked:false}
+         ];
+    }
+   
 }
