853fb99a5a5fb1f7ae25c2aa2724c4f4b65282cb
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-05-15 01:14:17 -0400

    Updating add User pages

--- src/app/users/users-add.component.ts
+++ src/app/users/users-add.component.ts
@@ -1,12 +1,23 @@
 import {Component,OnInit} from '@angular/core';
 import {Http} from '@angular/http';
+import {FormControl,FormGroup,FormBuilder,Validators} from '@angular/forms';
 @Component({
-    templateUrl:'./users-add.component.html'
+    templateUrl:'./users-add.component.html',
+    styleUrls:['../../assets/stylesheets/bootstrap.css',
+    '../../assets/stylesheets/styles.css']
 })
 export class UserAddComponent implements OnInit{
-
-constructor(private http:Http){
-
+userForm:FormGroup;
+constructor(fb:FormBuilder){
+this.userForm = fb.group({
+            name:['',Validators.compose([Validators.required])],
+            email:['',Validators.required]
+           , phone:['',Validators.required]
+            ,street:['',Validators.required]
+            ,suite:['',Validators.required]
+            ,city:['',Validators.required]
+            ,zipCode:['',Validators.required]
+        })
 }
 ngOnInit(){
 
