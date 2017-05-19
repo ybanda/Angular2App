66c53b1af3c66dadb9be6bf362e6f60dbf077462
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-05-19 00:29:30 -0400

    Updated selectors to fectch users and send the selected userId

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,43 +2,52 @@ import {Component,OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 
 import {PostService} from './posts.service';
+import {UserService} from '../users/users.service';
 import {Post,Comments} from './post';
 @Component({
     selector:'posts',
     templateUrl:'./posts.component.html',
     styleUrls:['../../assets/stylesheets/styles.css'],
-    providers:[PostService]
+    providers:[PostService,UserService]
 })
 export class PostsComponent implements OnInit{
     posts;
-    isLoading=true;
-    isClicked=false;
+    users;
+    userId;
+    isPostLoading=true;
+    isPostClicked=false;
     isCommentLoaded=true;
     post = new Post();
     comments:Comments[];
     
-    constructor(private postService:PostService){
+    constructor(private postService:PostService,
+                private userService:UserService){
 
     }
     ngOnInit(){
         this.postService.getPosts().then(posts=>{
             this.posts = posts,
-            this.isLoading=false;
+            this.isPostLoading=false;
             }
         );
+        this.userService.getUsers().subscribe(
+            users=>{this.users=users}
+         );
         
     }
     onPostSelect(postObj:Post){
         
         this.post = postObj;
-        this.isClicked=true;
+        this.isPostClicked=true;
        
         console.log('Post Selected ='+postObj.title +"..."+postObj.body);
         this.postService.getComments(postObj).then(comments=>{
             this.comments=comments,this.isCommentLoaded=false
         console.log(this.comments)})
-       
-        
-        
+     
+    }
+    onUserChange(user){
+        console.log(user.target.value);
+        this.userId=user.target.value;
     }
 }
