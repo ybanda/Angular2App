5f251dbb0b3ec8dbbe8772c5050645ec51572fe9
[Yashwanth Banda] <bandayashwanth@gmail.com>
2018-04-02 12:46:41 -0400

    Updated new Forms

---
+++ src/app/forms/new-course-form.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import { FormGroup, FormArray } from '@angular/forms';
+
+@Component({
+    selector:'new-course-form',
+    styleUrls:[''],
+    templateUrl:'./new-course-form.component.html'
+})
+export class NewCourseFormComponent{
+    
+    // Can be created like this.
+    
+    // form = new FormGroup({
+    //     username:new FormControl('',Validators.required),
+    //     password: new FormControl('',Validators.required)
+    // });
+
+    // (OR)
+
+    // constructor(fb:FormBuilder){
+    //     this.form = fb.group({
+    //             username:['',Validators.compose(
+    //                         [Validators.required, 
+    //                         UsernameValidators.cannotContainSpace,
+    //                         UsernameValidators.shouldBeUnique])],
+    //             password:['',Validators.required]
+    //         })
+    //     }
+
+    //( OR )
+    
+    form = new FormGroup({
+        topics: new FormArray([])
+    });
+
+    addTopic(topic:HTMLInputElement){
+
+    }
+}
